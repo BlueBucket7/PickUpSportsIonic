6c40afb998baafb115f8c1874db14138a2496a2b
BlueBucket7 <sjin.mailbox@gmail.com>
2015-10-12 13:20:32 -0700

    MVC re-structuring phase 1 complete. Controllers now separated by functionality

--- www/app.js
+++ www/app.js
@@ -1,24 +1,29 @@
-angular.module('pickup', ['ionic', 
-                          'pickup.controllers',
-                          'pickup.services',
-                          'ngCordova',
-                          'uiGmapgoogle-maps'
-                          ])
+angular.module('pickup', ['ionic',
+    'pickup.main.controllers',
+    'pickup.register.controller',
+    'pickup.home.controller',
+    'pickup.login.controller',
+    'pickup.map.controller',
+    'pickup.games.controller',
+    'pickup.services',
+    'ngCordova',
+    'uiGmapgoogle-maps'
+])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 .config(function(uiGmapGoogleMapApiProvider) {
@@ -30,79 +35,79 @@ angular.module('pickup', ['ionic',
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+    $stateProvider
 
-  .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'menu/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  // Home
-  .state('app.home', {
-    url: '/home',
-    views: {
-      'menuContent': {
-        templateUrl: 'home/home.html',
-        controller: 'HomeCtrl'
-      }
-    }
-  })
-
-  // Home Tabs
-  .state('app.home.map', {
-      url: "/map",
-      views: {
-        'map-tab': {
-          templateUrl: "map/map.html",
-          controller: 'MapCtrl'
-        }
-      }
+        .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'menu/menu.html',
+        controller: 'AppCtrl'
     })
-  
-  // Register
-  .state('app.register', {
-    url: '/register',
-    views: {
-      'menuContent': {
-        templateUrl: 'register/register.html',
-        controller: 'RegisterCtrl'
-      }
-    }
-  })
 
-  // Login
-  .state('app.login', {
-    url: '/login',
-    views: {
-      'menuContent': {
-        templateUrl: 'login/login.html',
-        controller: 'LoginCtrl'
-      }
-    }
-  })
+    // Home
+    .state('app.home', {
+        url: '/home',
+        views: {
+            'menuContent': {
+                templateUrl: 'home/home.html',
+                controller: 'HomeCtrl'
+            }
+        }
+    })
 
-  // My Games
-  .state('app.myGames', {
-    url: '/mygames',
-    views: {
-      'menuContent': {
-        templateUrl: 'games/my_games.html',
-        controller: 'MyGamesCtrl'
-      }
-    }
-  })
+    // Home Tabs
+    .state('app.home.map', {
+        url: "/map",
+        views: {
+            'map-tab': {
+                templateUrl: "map/map.html",
+                controller: 'MapCtrl'
+            }
+        }
+    })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'playlist/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('app/register');
+    // Register
+    .state('app.register', {
+        url: '/register',
+        views: {
+            'menuContent': {
+                templateUrl: 'register/register.html',
+                controller: 'RegisterCtrl'
+            }
+        }
+    })
+
+    // Login
+    .state('app.login', {
+        url: '/login',
+        views: {
+            'menuContent': {
+                templateUrl: 'login/login.html',
+                controller: 'LoginCtrl'
+            }
+        }
+    })
+
+    // My Games
+    .state('app.myGames', {
+        url: '/games',
+        views: {
+            'menuContent': {
+                templateUrl: 'games/my_games.html',
+                controller: 'GamesCtrl'
+            }
+        }
+    })
+
+    .state('app.single', {
+        url: '/playlists/:playlistId',
+        views: {
+            'menuContent': {
+                templateUrl: 'playlist/playlist.html',
+                controller: 'PlaylistCtrl'
+            }
+        }
+    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('app/register');
 });
